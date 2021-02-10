64e101c9a0c69ec323aae841eb211bf7f23174ae
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2021-02-10 17:08:21 -0600

    Starting Health Bar

---
+++ IceCreamTruck/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int Score;
+
+    private TextMeshProUGUI TMP;
+
+    private void Start()
+    {
+        TMP = GetComponent<TextMeshProUGUI>();
+    }
+    private void Update()
+    {
+        TMP.text = Score.ToString();
+    }
+}
